5fe141a9385d027d8be82beda0bef51637628747
Shivakant Shukla <shivakantshukla55@gmail.com>
2023-09-18 17:38:25 +0530

    hero section added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { Navbar } from "../components/Navbar";
+import { Hero } from "../components/Hero";
 
 export function Home() {
   return (
     <Container>
       <Navbar />
-      <div>hello world</div>
+      <Hero />
     </Container>
   );
 }
